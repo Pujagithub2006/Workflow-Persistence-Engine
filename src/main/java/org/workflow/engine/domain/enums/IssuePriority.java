35739877c71a97a78d59462a686a600ffe3f4d15
Puja Nikam <pujasantoshnikam@gmail.com>
2026-09-10 15:11:30 +0530

    feat: add Issue Priority enum objects with weight

--- src/main/java/org/workflow/engine/domain/enums/IssuePriority.java
+++ src/main/java/org/workflow/engine/domain/enums/IssuePriority.java
@@ -1,4 +1,24 @@
 package org.workflow.engine.domain.enums;
 
-public class IssuePriority {
+public enum IssuePriority {
+    TRIVIAL(0),
+    MINOR(1),
+    MAJOR(2),
+    CRITICAL(3),
+    BLOCKER(4);
+
+    private final int level;
+
+    IssuePriority(int level) {
+        this.level = level;
+    }
+
+    private int getLevel() {
+        return level;
+    }
+
+    public boolean isHigherThan(IssuePriority otherPrio) {
+        return this.level > otherPrio.level;
+    }
+
 }
