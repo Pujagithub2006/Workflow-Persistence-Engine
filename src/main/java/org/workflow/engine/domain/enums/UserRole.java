c7382a8ab89ac486f4cfabc8ccd8bf652615d20a
Puja Nikam <pujasantoshnikam@gmail.com>
2026-09-10 15:13:29 +0530

    feat: add User role enum objects and public RBAC methods

--- src/main/java/org/workflow/engine/domain/enums/UserRole.java
+++ src/main/java/org/workflow/engine/domain/enums/UserRole.java
@@ -1,4 +1,18 @@
 package org.workflow.engine.domain.enums;
 
-public class UserRole {
+public enum UserRole {
+    ADMIN,
+    PROJECT_LEAD,
+    DEVELOPER,
+    TESTER,
+    VIEWER;
+
+    public boolean canManageProject() {
+        return this==ADMIN || this==PROJECT_LEAD;
+    }
+
+    public boolean canEditIssues() {
+        return this!=VIEWER;
+    }
+
 }
