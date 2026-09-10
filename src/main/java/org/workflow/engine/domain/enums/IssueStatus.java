64249d1974a2f94098c07ac553352ce433e1e339
Puja Nikam <pujasantoshnikam@gmail.com>
2026-09-10 15:11:59 +0530

    feat: add Issue Status enum objects

--- src/main/java/org/workflow/engine/domain/enums/IssueStatus.java
+++ src/main/java/org/workflow/engine/domain/enums/IssueStatus.java
@@ -1,4 +1,15 @@
 package org.workflow.engine.domain.enums;
 
-public class IssueStatus {
+public enum IssueStatus {
+    TO_DO,
+    IN_PROGRESS,
+    IN_REVIEW,
+    DONE,
+    CLOSED,
+    REOPENED;
+
+    public boolean isTerminal() {
+        return this==DONE || this==CLOSED;
+    }
+
 }
